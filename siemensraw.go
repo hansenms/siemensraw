0b11ce243b70659b1449eaa524b1b5fbef50306d
Hui Xue <hui.xue@nih.gov>
2016-09-07 11:09:06 -0400

    add ismrmrd signature

--- siemensraw.go
+++ siemensraw.go
@@ -5,7 +5,9 @@ import(
 	"bufio"
 	"crypto/sha1"
 	"encoding/binary"
+    "encoding/base64"
 	"fmt"
+    "io"
 	"io/ioutil"
 	"os"
 	"path"
@@ -47,15 +49,70 @@ func StripString(str string) string {
 		if r == '"' {
 			return -1
 		}
+        if r == '\n' {
+			return -1
+		}
+        if r == '\t' {
+			return -1
+		}
 		return r
 	}, str)
 }
 
 func HashBuffer(str string) []byte {
         cleanString := StripString(str)
-	hasher := sha1.New()
-	hasher.Write([]byte(cleanString[:]))
-	return hasher.Sum(nil)
+        hasher := sha1.New()
+        hasher.Write([]byte(cleanString[:]))
+        return hasher.Sum(nil)
+}
+
+func GetPhoenixFromISMRMRD(filename string) string {
+    fi, err := os.Open(filename)
+    if err != nil {
+        panic(err)
+    }
+    
+    r := bufio.NewReader(fi)  
+        
+    defer func() {
+        if err := fi.Close(); err != nil {
+            panic(err)
+        }
+    }()
+    
+    // read in a part of ismrmrd file and parse it
+    n_bytes := 4096*1024
+    buf_bytes := make([]byte, n_bytes)   
+    n, err := r.Read(buf_bytes)
+    if err != nil && err != io.EOF {
+        panic(err)
+    }
+    
+    // fmt.Println("Read in bytes \n", n)
+    
+    buf := string(buf_bytes[:])  
+    idx := strings.Index(buf, "<name>SiemensBuffer_Phoenix</name>")  
+    idx2 := strings.Index(buf[idx:n-1], "</value>") 
+    buf2 := string(buf[idx:idx+idx2])
+    idx3 := strings.Index(buf2, "<value>")
+    
+    base64_str := buf2[idx3+len("<value>"):len(buf2)]   
+    
+    // decode, base64
+    data, err := base64.StdEncoding.DecodeString(base64_str)
+	if err != nil {
+		panic(err)
+	}
+    
+    phoenixprot := string(data[:])
+
+    idx = strings.Index(phoenixprot, "### ASCCONV BEGIN")
+	idx2 = strings.Index(phoenixprot, "### ASCCONV END ###")
+	phoenixprot = string(phoenixprot[idx:idx2+len("### ASCCONV END ###")])
+   
+    // fmt.Println(phoenixprot)
+    
+    return phoenixprot
 }
 
 func GetPhoenixFromDicom(filename string) string {
@@ -68,7 +125,7 @@ func GetPhoenixFromDicom(filename string) string {
 	asstring := string(bytes[:])
 	idx := strings.Index(asstring, "<Name> \"PhoenixMetaProtocol\"")
 	asstring = string(bytes[idx:])
-	idx = strings.Index(asstring, "<XProtocol>")
+	idx = strings.Index(asstring, "### ASCCONV BEGIN")
 	idx2 := strings.Index(asstring, "### ASCCONV END ###")
 	phoenixprot := string(asstring[idx:idx2+len("### ASCCONV END ###")])
 	return phoenixprot
@@ -81,7 +138,10 @@ func DatFileSignature(filename string) string {
 
 	for _, b := range rawfileinfo[len(rawfileinfo)-1].Buffers {
 		if b.Name == string("Phoenix") {
-			hash = fmt.Sprintf("%x", HashBuffer(b.Buffer))
+            idx := strings.Index(b.Buffer, "### ASCCONV BEGIN")
+            idx2 := strings.Index(b.Buffer, "### ASCCONV END ###")
+            phoenixprot := string(b.Buffer[idx:idx2+len("### ASCCONV END ###")])
+			hash = fmt.Sprintf("%x", HashBuffer(phoenixprot))
 		}	
 	}
 	return hash
